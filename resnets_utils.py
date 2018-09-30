1d690c63294d5f9b2b5a30109a8c8f0957508937
ambareeshsrja16 <39631487+ambareeshsrja16@users.noreply.github.com>
2018-09-30 16:53:11 -0700

    Update resnets_utils.py

--- resnets_utils.py
+++ resnets_utils.py
@@ -6,12 +6,12 @@ import math
 
 def load_dataset():
     train_dataset = h5py.File('datasets/train_signs.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) #  train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) #  train set labels
 
     test_dataset = h5py.File('datasets/test_signs.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) #  test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) #  test set labels
 
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
     
